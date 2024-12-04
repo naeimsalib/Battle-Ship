6cf9b41c86d93ddeec36f24f76d23d4b2b87e17a
naeimsalib <naeimsalib@yahoo.com>
2024-12-04 15:40:02 -0500

    Fixed audio path links

--- js/script.js
+++ js/script.js
@@ -8,13 +8,15 @@ const flipButton = document.querySelector(".flip-btn");
 const playButton = document.querySelector(".play-btn");
 const pauseButton = document.querySelector(".pause-btn");
 const soundButton = document.querySelector(".sound-btn");
-const buttonsAudio = new Audio("/Assets/Audio/buttonsSound.mp3");
-const playerLoses = new Audio("/Assets/Audio/player-loses.mp3");
-const playerWins = new Audio("/Assets/Audio/player-wins.mp3");
-const hitAudio = new Audio("/Assets/Audio/hitAudio.mp3");
-const missedAudio = new Audio("/Assets/Audio/missedAudio.mp3");
-const startBattleAudio = new Audio("/Assets/Audio/startBattleAudio.mp3");
 
+const buttonsAudio = new Audio("Battle-Ship/Assets/Audio/buttonsSound.mp3");
+const playerLoses = new Audio("Battle-Ship/Assets/Audio/game-over.mp3");
+const playerWins = new Audio("Battle-Ship/Assets/Audio/player-wins.mp3");
+const hitAudio = new Audio("Battle-Ship/Assets/Audio/hitAudio.mp3");
+const missedAudio = new Audio("Battle-Ship/Assets/Audio/missedAudio.mp3");
+const startBattleAudio = new Audio("Battle-Ship/Assets/Audio/startBattleAudio.mp3");
+
+// Debugging: Check if audio files are loaded
 console.log(buttonsAudio);
 console.log(playerLoses);
 console.log(playerWins);
